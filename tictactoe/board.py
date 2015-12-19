ed29157284edf30fc62c46455a5252edd4de1fd2
cassioss <cassiosantossousa@gmail.com>
2015-12-19 17:45:24 -0200

    Added a few more methods to access the game board

--- tictactoe/board.py
+++ tictactoe/board.py
@@ -2,16 +2,18 @@
 # Adds commands to test how the tic-tac-toe board works, changes its tiles, is printed and reset.
 #
 
-# Restarts a game board, which is assumed to be rectangular.
+# Class resembling a game board, which is assumed to be rectangular.
 
 
 class Board():
     rows = 3
     cols = 3
     grid = [[]]
+    available_plays = []
 
     def __init__(self):
         self.grid = [[None for y in range(self.cols)] for x in range(self.rows)]
+        self.available_plays = range(0, 9)
 
     def __str__(self):
         output = "\n"
@@ -30,11 +32,16 @@ class Board():
 
     def play_at(self, x, y, symbol):
         self.grid[x][y] = symbol
+        self.available_plays.remove(x * 3 + y)
 
     def restart(self):
         for x in range(self.rows):
             for y in range(self.cols):
                 self.grid[x][y] = None
+        self.available_plays = range(0, 9)
 
     def can_play_at(self, x, y):
-        return self.grid[x][y] is None
+        return self.available_plays.__contains__(x * 3 + y)
+
+    def play_to_pos(self, play):
+        return self.grid[play / 3][play % 3]
--- tictactoe/board_test.py
+++ tictactoe/board_test.py
@@ -18,6 +18,13 @@ class WinningTests(unittest.TestCase):
         test_board_2.restart()
         assert test_board_2.can_play_at(0, 0)
 
+    def test_play_conversion(self):
+        assert test_board_1.play_to_pos(0) == test_board_1.grid[0][0]
+        assert test_board_1.play_to_pos(2) == test_board_1.grid[0][2]
+        assert test_board_1.play_to_pos(4) == test_board_1.grid[1][1]
+        assert test_board_1.play_to_pos(5) == test_board_1.grid[1][2]
+        assert test_board_1.play_to_pos(7) == test_board_1.grid[2][1]
+
 
 def main():
     unittest.main()
